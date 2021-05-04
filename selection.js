e9985e8268028547c968e8dd9c2a802b2babc46e
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-03 17:54:13 -0700

    Implemented selectionSort(arr) in selection.js. Also added swap(arr, i, j) in selection.js.

--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minimum = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minimum]) {
+        minimum = j;
+      }
+    }
+    swap(arr, i, minimum);
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
