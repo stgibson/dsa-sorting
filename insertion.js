f7450cd273183e0a32e4ba9fdf70613febafe1bc
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-03 17:48:48 -0700

    Implemented insertionSort(arr) in insertion.js. Also added swap(arr, i, j) in insertion.js.

--- insertion.js
+++ insertion.js
@@ -1,3 +1,18 @@
-function insertionSort() {}
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let j = i + 1;
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      swap(arr, j - 1, j);
+      j--;
+    }
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
