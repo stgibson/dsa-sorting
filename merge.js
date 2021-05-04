14adf3e902eaf8578d68a448de379a964bce1ad6
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-03 17:36:52 -0700

    Implemented merge(arr1, arr2) & mergeSort(arr). Modified merge.js.

--- merge.js
+++ merge.js
@@ -1,5 +1,36 @@
-function merge() {}
+function merge(arr1, arr2) {
+  const newArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      newArr.push(arr1[i]);
+      i++;
+    }
+    else {
+      newArr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    newArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    newArr.push(arr2[j]);
+    j++;
+  }
+  return newArr;
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const median = Math.floor(arr.length / 2);
+  const leftArr = mergeSort(arr.slice(0, median));
+  const rightArr = mergeSort(arr.slice(median));
+  return merge(leftArr, rightArr);
+}
 
 module.exports = { merge, mergeSort};
