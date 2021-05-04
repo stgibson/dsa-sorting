0b67da51267031d37818d510a3deed277b4f315d
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-03 17:24:57 -0700

    Implemented bubbleSort(arr) in bubble.js. Also added swap(arr, i, j) in bubble.js & modified package.json to use jest.

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let swapped = false;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      return arr;
+    }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
