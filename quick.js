2f0cc140d39ddf2e55125d4f7055335ab22be617
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-03 20:41:48 -0700

    Implemented quick sort. Implemented pivot(arr, start = 0, end = arr.length - 1) & quickSort(arr, left = 0, right = arr.length - 1) in quick.js & added swap(arr, i, j) in quick.js.

--- quick.js
+++ quick.js
@@ -1,14 +1,38 @@
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
 /*
 pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1) {
+  let pivotIndex = start;
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < arr[start]) {
+      pivotIndex++;
+      swap(arr, i, pivotIndex);
+    }
+  }
+  swap(arr, start, pivotIndex);
+  return pivotIndex;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left >= right) {
+    return arr;
+  }
+  const pivotIndex = pivot(arr, left, right);
+  quickSort(arr, left, pivotIndex - 1);
+  quickSort(arr, pivotIndex + 1, right);
+  return arr;
+}
 
-module.exports = quickSort;
+module.exports = { pivot, quickSort };
